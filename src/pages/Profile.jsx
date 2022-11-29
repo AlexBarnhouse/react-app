bbf4c45d9298cf9f2be3c0f94e4b1ddf0b022f4e
AlexBarnhouse <aragon-1995@mail.ru>
2022-11-29 22:08:37 +0500

    lesson-5

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,16 @@
+import { useDispatch } from 'react-redux'
+
 const Profile = () => {
+
+    const dispatch = useDispatch();
+    const onChecked = () => {
+        dispatch({ type: 'PUSH' })
+    };
+
     return (
         <div>
             <p>Profile</p>
+            <input type="checkbox" onClick={onChecked} />
         </div>
     );
 }
